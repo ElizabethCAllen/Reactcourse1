4dbbdc95544ca6ceaaea50f6c69d5d07c998f8a2
Elizabeth <elizabethallen36@gmail.com>
2021-03-02 21:18:37 -0800

    finished day 3 and pushing ther code, will later go and add teach's code into the files from his repo for reference purposes

--- reactcourse1/src/App.js
+++ reactcourse1/src/App.js
@@ -1,8 +1,35 @@
 
 import './App.css';
-import ToDoList from './todolist'
-
+import React from 'react'
+import getMoviesByName from 
+// import ToDoList from './todolist'
 // import {MovieCard } from './MovieCard'
+class App extends React.component{
+  constructor(props){
+    super(props);
+    this.setState={
+      search: 'batman',
+      isLoading: false,
+      movies:[],
+      error:null,
+    }
+  }
+  
+componentDidMount(){
+ tis.setState({
+   isLoading:true
+ });
+
+ getMoviesByName(this.state.search)
+ .then(data =>{
+   this.setState({
+     isLoadfing:false,
+     movies:data,
+     
+   })
+ })
+}
+}
 // function App() {
 // <>
 //   return (
--- reactcourse1/src/todolist.js
+++ reactcourse1/src/todolist.js
@@ -1,47 +1,47 @@
 
-class ToDoList extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            todos: ["learn react", "become dev"],
-            goals: ["make the muns"],
-            numberofTodos: 3,
-            getGreeting: ()=> "yo"
-        };
-    componentDidMount() {
-        console.log("hey I mounted");
-        setInterval(() => {
-            this.setState({
-                currentTime: new Date().toLocaleTimeString()
-            });
-        }, 1000);
-    }
-    componentDidUpdate(){
-        console.log("I am updating" );
-    }
-    componentWillUnmount(){
-        console.log("Imma go die now, bye UWU");
-    }
-    shouldComponentUpdate(){
-        console.log("hey, should I update?");
-        // supposed to return a boolean
-    }
-    }
+// class ToDoList extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             todos: ["learn react", "become dev"],
+//             goals: ["make the muns"],
+//             numberofTodos: 3,
+//             getGreeting: ()=> "yo"
+//         };
+//     componentDidMount() {
+//         console.log("hey I mounted");
+//         setInterval(() => {
+//             this.setState({
+//                 currentTime: new Date().toLocaleTimeString()
+//             });
+//         }, 1000);
+//     }
+//     componentDidUpdate(){
+//         console.log("I am updating" );
+//     }
+//     componentWillUnmount(){
+//         console.log("Imma go die now, bye UWU");
+//     }
+//     shouldComponentUpdate(){
+//         console.log("hey, should I update?");
+//         // supposed to return a boolean
+//     }
+//     }
 
-    render(){
+//     render(){
         
-        const { todos, goals, getGreeting, numberofTodos} = 
-        this.state;
-        // below is a way not to cal setState
+//         const { todos, goals, getGreeting, numberofTodos} = 
+//         this.state;
+//         // below is a way not to cal setState
 
 
 
-        return (
-        <div>
-        <p>{this.state.getGreeting()}</p>
-        <p> i have {goals.length} goalsd to complete</p>
-        </div>
-        )
-    }
-}
-export default Todolist
+//         return (
+//         <div>
+//         <p>{this.state.getGreeting()}</p>
+//         <p> i have {goals.length} goalsd to complete</p>
+//         </div>
+//         )
+//     }
+// }
+// export default Todolist
