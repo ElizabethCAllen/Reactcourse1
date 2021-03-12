03ba44919e264ee4cd5b5e1b92455f2f4b6dcfb1
Elizabeth <elizabethallen36@gmail.com>
2021-03-11 20:10:35 -0800

    added the npm bootstrap package, andtried to add thecode from the lesson, will add screenshots of the lessopn code later

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import"../css/styles.css"
+
+ const Button =({type = "primary"}) => {
+     const buttonStyle = {
+         backgroundColor: type === "primary" ? "#01480f" :"#0047ab",
+         padding
+     }
+ }
---
+++ js/button.js
@@ -0,0 +1,11 @@
+import "../css/button.css";
+
+const Button=({ type = "primary"})=> {
+    backgroundColor:
+};
+
+// return(
+//     <button className={`btn ${type = ""}`}=> {
+//         return <button> click md<</button>;
+//     };
+// );
